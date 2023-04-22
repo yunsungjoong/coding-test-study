c0ff2868abb2393782136417d351d5890ea48f5b
Y-SungJoong <yun_sj94@naver.com>
2023-04-22 17:17:23 +0900

    추가

---
+++ 14.가장 긴 문자열.js
@@ -0,0 +1,13 @@
+function soloution(s){
+    let answer, max=Number.MIN_SAFE_INTEGER;
+    for(let x of s){
+        if(x.length>max){
+            max=x.length;
+            answer=x;
+        }
+    }
+    return answer;
+}
+
+let str=["teacher", "time", "student", "beautiful", "good"];
+console.log(soloution(str));
