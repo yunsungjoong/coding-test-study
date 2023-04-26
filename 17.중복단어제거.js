df530d2932e6e992cfc6c1bd465716d1cacaae60
Y-SungJoong <yun_sj94@naver.com>
2023-04-26 23:05:32 +0900

    추가

---
+++ 17.중복단어제거.js
@@ -0,0 +1,13 @@
+function solution(s){
+    let answer;
+    // console.log(s.indexOf("time"));
+    answer=s.filter(function(v,i){
+        // if(s.indexOf(v)===i) return true;
+            return s.indexOf(v)===i;
+    });
+    return answer;
+
+}
+
+let str=["good", "time", "good", "time", "student"];
+console.log(solution(str));
