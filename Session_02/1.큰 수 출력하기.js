b4bf1caa2420c1acc178c1ef655de3a380ab01aa
yunsungjoong <yun_sj94@naver.com>
2023-04-27 23:42:58 +0900

    추가 및 수정

--- Session_02/1.큰 수 출력하기.js
+++ Session_02/1.큰 수 출력하기.js
@@ -1,5 +1,9 @@
 function soloution(arr){
     let answer=[];
+    answer.push(arr[0]);
+    for(let i=1; i<arr.length; i++){
+        if(arr[i]>arr[i-1]) answer.push(arr[i]);
+    }
 
     return answer;
 }
