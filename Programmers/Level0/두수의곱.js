ba65603badbec96da0925f624e261e2b6d27bb56
yunsungjoong <yun_sj94@naver.com>
2023-11-09 19:48:10 +0900

    프로그래머스 레벨 0 시작

---
+++ Programmers/Level0/두수의곱.js
@@ -0,0 +1,6 @@
+/* 
+문제 num1 , num2가 매개변수 주어진다. num1 과 num2 를 곱한 값을 return 하도록 solution 함수를 완성하시오 
+*/
+
+// 정답 
+const solution = (num1,num2) => num1*num2
---
+++ Programmers/Level0/몫구하기.js
@@ -0,0 +1,6 @@
+/* 
+문제 설명
+정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+*/
+
+const solution = (num1,num2) => Math.floor(num1,num2);
