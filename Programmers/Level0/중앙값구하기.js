beb8932624f050a6c2492f5eaa9efa36c413dc0f
yunsungjoong <yun_sj94@naver.com>
2023-11-23 14:55:56 +0900

    중앙값구하기

---
+++ Programmers/Level0/중앙값구하기.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+    // 배열 길이 중 중앙값 (중간 index) 찾기
+    const mid_num = Math.floor(arr.length / 2);
+  
+    // 오름차순으로 정렬하기 - sort() 메서드 사용
+    const asc_arr = arr.sort((a, b) => a - b);
+  
+    // 오름차순된 새로운 배열 중 중간 index 반환하기
+    return asc_arr[mid_num];
+}
+
+
+function solution(arr){
+    return arr.sort((a , b) => a - b)[parseInt(array.length / 2)];
+    
+}
