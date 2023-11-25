dea57ea00a749d4852fdd60271fdb388e5ebc68e
yunsungjoong <yun_sj94@naver.com>
2023-11-25 15:27:04 +0900

    최빈값 구하기

---
+++ Programmers/Level0/최빈값구하기.js
@@ -0,0 +1,10 @@
+function solution(array) {
+    let objCount = array.reduce((result, curr, idx) => {
+        result[curr] = (result[curr] || 0) + 1
+        return result
+    }, {})
+    let sortedKeys = Object.keys(objCount).sort((a, b) => objCount[b] - objCount[a])
+    
+    if (objCount[sortedKeys[0]] === objCount[sortedKeys[1]]) return -1
+    else return Number(sortedKeys[0])
+}
