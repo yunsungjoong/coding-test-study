922297bb8a1807f8596fcd8795f6c0c30b41c7c0
yunsungjoong <yun_sj94@naver.com>
2023-12-27 21:02:23 +0900

    수정

--- Programmers/Level0/아메리카노.js
+++ Programmers/Level0/아메리카노.js
@@ -1,6 +1,3 @@
-function solution(money){
-    let answer = [];
-    answer.push(parserInt(money / 5500))
-    answer.push(money % 5500)
-    return answer
-}   
+function solution(money) {
+    return [Math.floor(money / 5500), money % 5500];
+}
